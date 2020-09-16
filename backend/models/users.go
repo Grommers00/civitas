912d5b60a30c16988934d3850f7008962781a5ba
Grommers00 <james.inkster@live.ca>
2020-09-15 20:42:36 -0400

    Updated to please hound

--- backend/models/users.go
+++ backend/models/users.go
@@ -1,6 +1,6 @@
 package civitas
 
-//This will be for the users that will be using the ladder as a basic starting off point.
+//Users will be for the users that will be using the ladder as a basic starting off point.
 type Users struct {
 	Account string `json:"title"`
 	Desc    string `json:"desc"`
