f47b6ee312609ad6f3bd7b5a4d9c869f949f69cb
Grommers00 <james.inkster@live.ca>
2020-09-15 21:20:49 -0400

    Updated last one.

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,6 +13,11 @@ import (
 // ApplicationConfiguration contains all the ENV variables that will be used within the backend
 type ApplicationConfiguration struct {
 	Port string
+}
+
+// HelloWorld will be our first object passed through.
+type HelloWorld struct {
+	Message string `json:"Message"`
 }
 
 // GoDotEnvVariable gets a list of all the variables into the handle requests
@@ -37,17 +43,26 @@ func HomePage(w http.ResponseWriter, r *http.Request) {
 
 // FlutterInitRequest TODO: This is a test API for Flutter Integration, Temp.sends "Hello, World". Meta.
 func FlutterInitRequest(w http.ResponseWriter, r *http.Request) {
+	message := HelloWorld{Message: "Hello, World!"}
 
+	js, err := json.Marshal(message)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
 
 //HandleRequests listens for any requests that come in.
 func HandleRequests(applicationConfig ApplicationConfiguration) {
 
 	http.HandleFunc("/", HomePage)
+	http.HandleFunc("/flutter", FlutterInitRequest)
 	log.Fatal(http.ListenAndServe(applicationConfig.Port, nil))
 }
 
 func main() {
-	applicationConfig := goDotEnvVariable()
+	applicationConfig := GoDotEnvVariable()
 	HandleRequests(applicationConfig)
 }
