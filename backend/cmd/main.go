55d0ec67c8462e197ac7ea21825fac537e84c253
raygervais <raygervais@hotmail.ca>
2020-11-09 21:56:28 -0500

    adds: github.com/awslabs/aws-lambda-go-api-proxy/ integration idea with mux

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"context"
+	"log"
 	"os"
-
-	handlers "github.com/grommers00/civitas/pkg"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -11,11 +11,29 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"github.com/awslabs/aws-lambda-go-api-proxy/gorillamux"
+	"github.com/gorilla/mux"
 )
 
 var (
 	dynaClient dynamodbiface.DynamoDBAPI
+	muxLambda  *gorillamux.GorillaMuxAdapter
 )
+
+func init() {
+	// stdout and stderr are sent to AWS CloudWatch Logs
+	log.Printf("Mux: Cold Start")
+
+	r := mux.NewRouter()
+
+	// routes.ConnectNewsSubrouter(r)
+	// routes.ConnectProfileSubrouter(r)
+	// routes.ConnectLeaguesSubrouter(r)
+	// routes.ConnectStandingSubrouter(r)
+	// routes.ConnectSeasonsRouter(r)
+
+	muxLambda = gorillamux.New(r)
+}
 
 func main() {
 	region := os.Getenv("AWS_REGION")
@@ -25,23 +43,38 @@ func main() {
 	if err != nil {
 		return
 	}
+
 	dynaClient = dynamodb.New(awsSession)
-	lambda.Start(handler)
+	lambda.Start(Handler)
 }
 
 const tableName = "civitas-dev"
 
-func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	switch req.HTTPMethod {
-	case "GET":
-		return handlers.GetProfile(req, tableName, dynaClient)
-	case "POST":
-		return handlers.CreateProfile(req, tableName, dynaClient)
-	case "PUT":
-		return handlers.UpdateProfile(req, tableName, dynaClient)
-	case "DELETE":
-		return handlers.DeleteProfile(req, tableName, dynaClient)
-	default:
-		return handlers.UnhandledMethod()
-	}
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// If no name is provided in the HTTP request body, throw an error
+	return muxLambda.ProxyWithContext(ctx, req)
 }
+
+// func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+// 	switch req.Path {
+// 	case "profile":
+// 		return ProfileHandlers(req)
+// 	default:
+// 		return handlers.UnhandledMethod()
+// 	}
+// }
+
+// func ProfileHandlers(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+// 	switch req.HTTPMethod {
+// 	case "GET":
+// 		return handlers.GetProfile(req, tableName, dynaClient)
+// 	case "POST":
+// 		return handlers.CreateProfile(req, tableName, dynaClient)
+// 	case "PUT":
+// 		return handlers.UpdateProfile(req, tableName, dynaClient)
+// 	case "DELETE":
+// 		return handlers.DeleteProfile(req, tableName, dynaClient)
+// 	default:
+// 		return handlers.UnhandledMethod()
+// 	}
+// }
--- backend/go.mod
+++ backend/go.mod
@@ -5,7 +5,12 @@ go 1.15
 require (
 	github.com/aws/aws-lambda-go v1.20.0
 	github.com/aws/aws-sdk-go v1.35.24
-	github.com/gorilla/mux v1.8.0
-	github.com/joho/godotenv v1.3.0
-	github.com/stretchr/testify v1.6.1
+	github.com/awslabs/aws-lambda-go-api-proxy v0.8.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	golang.org/x/net v0.0.0-20201109172640-a11eb1b685be // indirect
+	golang.org/x/text v0.3.4 // indirect
+	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
