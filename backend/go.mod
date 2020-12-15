1546ff87fa9ee205668c19d02388d21436c89c51
James Inkster <42331013+Grommers00@users.noreply.github.com>
2020-12-15 17:45:54 -0500

    re-organized (#27)
    * re-organized
    
    * removed main.go go.mod go.sum

--- backend/go.mod
+++
@@ -1,9 +0,0 @@
-module github.com/grommers00/civitas/backend
-
-go 1.15
-
-require (
-	github.com/gorilla/mux v1.8.0
-	github.com/joho/godotenv v1.3.0
-	github.com/stretchr/testify v1.6.1
-)
--- backend/ray_build.sh
+++ backend/ray_build.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/bash -e
+
+# Init Sam if needed
+make
 
 cd functions
 
@@ -7,5 +10,11 @@ for dir in *; do
     cd ./$dir/
     CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build
     cp $dir "../../.aws-sam/build/${dir^}Handler/"
+    rm -f $dir
     cd ..
 done
+
+cd ..
+
+# Start Local API
+sam local start-api
